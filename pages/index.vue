0715fab8f497450bd6b775afcf43311a8f029c13
Artur <arjos.eu@outlook.com>
2023-02-04 11:47:31 -0300

    Navigation started

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,18 @@
   <div style="padding-top: 80px;
               min-height: 500px;"
        class="text-center">
-    Sou o INDEX
+    <h1>INDEX Page</h1>
+    <div class="row">
+      <div class="col-12">
+        <NuxtLink to="/other">
+          Go to Other page
+        </NuxtLink>
+      </div>
+      <div class="col-12">
+        <NuxtLink to="/custom">
+          Go to Custom page
+        </NuxtLink>
+      </div>
+    </div>
   </div>
 </template>
